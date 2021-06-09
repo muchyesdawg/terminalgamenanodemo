5e9f17f91f561c5eea40eff2de36d44436b11056
muchyesdawg <75879686+muchyesdawg@users.noreply.github.com>
2021-06-09 22:41:22 +0800

    c# base

---
+++ terminalnanorebellionc#/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+//this is made by markus au
+namespace c__nanorebellion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Nanorebellion";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("what class are you? (type help for help)");
+            var action = Console.ReadLine();
+        }
+    }
+}
